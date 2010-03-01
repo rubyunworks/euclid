c7be751a10f25b0440b81b75a7ae3cdd11163aeb
7rans <transfire@gmail.com>
2010-03-01 16:58:46 -0500

    first commit

---
+++ lib/euclid.rb
@@ -0,0 +1,1 @@
+Dir[File.dirname(__FILE__) + '/euclid/*.rb'].each{ |lib| require lib }
---
+++ lib/euclid/math.rb
@@ -0,0 +1,5 @@
+require 'euclid/math/probability'
+require 'euclid/math/trigonometry'
+require 'euclid/math/finance'
+require 'euclid/math/miscellaneous'
+
